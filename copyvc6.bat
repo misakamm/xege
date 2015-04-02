ae7f97cabef2ee6859dfcdfeb9ecd2ddaff31ad1
misakamm <misakamm@gmail.com>
2015-04-02 16:31:22 +0800

    fix vc6 pub script

--- copyvc6.bat
+++ copyvc6.bat
@@ -5,7 +5,7 @@ set pubpath=pub/%egever%/bin
 copy include\* vc6\VC98\Include\
 mkdir vc6\VC98\Include\ege
 copy include\ege\* vc6\VC98\Include\ege\
-copy lib\vc6\* vc6\VC98\Lib\
+copy lib\vc6\lib\* vc6\VC98\Lib\
 mkdir "%pubpath%"
 del /Q "%pubpath%\vc6_ege%egever%.7z"
 7z a -t7z -bd -mx=9 -ms=on "%pubpath%\vc6_ege%egever%.7z" "vc6\*"
