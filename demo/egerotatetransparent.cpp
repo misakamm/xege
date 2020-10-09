61b96d9baa22a236e211bb1106d0af8e48182f4a
Roy Qu <royqh1979@gmail.com>
2020-10-09 12:21:02 +0800

    fix indent and manual error

--- demo/egerotatetransparent.cpp
+++ demo/egerotatetransparent.cpp
@@ -2,28 +2,31 @@
 
 int main()
 {
-    int points[6];
-    initgraph( 640, 480 );
-    setbkcolor(WHITE);
-    setcolor( BLACK);
-    setfillcolor( RED );
-    
-    bar(50,50,600,400);    
+	int points[6];
+	initgraph( 640, 480 );
+	// draw background image
+	setbkcolor(WHITE);
+	setcolor( BLACK);
+	setfillcolor( BLACK );
+	
+	bar(50,50,600,400);	
    
-    PIMAGE  img = newimage(200,200);
-    setbkcolor(WHITE,img);
-    setcolor(RED,img);
-    setfillcolor(YELLOW,img);
-
-    points[0]=100;
-    points[1]=50;
-    points[2]=50;
-    points[3]=150;
-    points[4]=150;
-    points[5]=150;
-    fillpoly(3,points,img);
-
-    putimage_rotatetransparent(NULL,img,300,200,100,100,WHITE,45.0/180*PI,2);
-    getch();
-    return 0;
+   	//image to be rotated and put
+	PIMAGE  img = newimage(200,200);
+	setbkcolor(WHITE,img);
+	setcolor(RED,img);
+	setfillcolor(YELLOW,img);
+	
+	points[0]=100;
+	points[1]=50;
+	points[2]=50;
+	points[3]=150;
+	points[4]=150;
+	points[5]=150;
+	fillpoly(3,points,img);
+	
+	//put and rotate
+	putimage_rotatetransparent(NULL,img,300,200,100,100,WHITE,45.0/180*PI,2);
+	getch();
+	return 0;
 }
