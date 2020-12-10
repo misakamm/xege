770533e44e216a79c9ed960760269ec628c6615d
royqh1979@gmail.com <Roy>
2020-12-10 19:50:30 +0800

    修正对齐问题

--- src/egegapi.cpp
+++ src/egegapi.cpp
@@ -50,12 +50,12 @@ static FILE fOldOut;
 BOOL init_console(){
 	HMENU hMenu;
 	int hCrt;
-    FILE *hf;
+	FILE *hf;
 	if (hInput!=NULL) {
 		return FALSE;
 	}
 	if (GetConsoleWindow()!=NULL) {
-        return FALSE;
+		return FALSE;
 	}
 	if (!AllocConsole()) {
 		return FALSE;
@@ -66,33 +66,33 @@ BOOL init_console(){
 		return FALSE;
 	}
 	hInput=
-        GetStdHandle(STD_INPUT_HANDLE);
+		GetStdHandle(STD_INPUT_HANDLE);
 	if(INVALID_HANDLE_VALUE == hInput) {
 		return FALSE;
 	}
-    SetConsoleTitle("EGE CONSOLE");
-    hConsoleWnd=GetConsoleWindow();
+	SetConsoleTitle("EGE CONSOLE");
+	hConsoleWnd=GetConsoleWindow();
  	if(INVALID_HANDLE_VALUE == hConsoleWnd) {
 		return FALSE;
 	}
-    hMenu = GetSystemMenu(hConsoleWnd,0);
-    if(hMenu != NULL)
-    {
-        DeleteMenu(hMenu, SC_CLOSE, MF_BYCOMMAND);
-        DrawMenuBar(hConsoleWnd);
-    }
+	hMenu = GetSystemMenu(hConsoleWnd,0);
+	if(hMenu != NULL)
+	{
+		DeleteMenu(hMenu, SC_CLOSE, MF_BYCOMMAND);
+		DrawMenuBar(hConsoleWnd);
+	}
 
 	hCrt=_open_osfhandle((long)hOutput,_O_TEXT);
-    hf=_fdopen( hCrt, "w" );
-    fOldOut=*stdout;
-    *stdout=*hf;
-    setvbuf( stdout, NULL, _IONBF, 0 );
+	hf=_fdopen( hCrt, "w" );
+	fOldOut=*stdout;
+	*stdout=*hf;
+	setvbuf( stdout, NULL, _IONBF, 0 );
 
-    hCrt=_open_osfhandle((long)hInput,_O_TEXT);
-    hf=_fdopen(hCrt, "r");
-    fOldIn=*stdin;
-    *stdin=*hf;
-    //setvbuf( stdin, NULL, _IONBF, 0 );
+	hCrt=_open_osfhandle((long)hInput,_O_TEXT);
+	hf=_fdopen(hCrt, "r");
+	fOldIn=*stdin;
+	*stdin=*hf;
+	//setvbuf( stdin, NULL, _IONBF, 0 );
   // test code
 
 
@@ -106,71 +106,71 @@ void clear_console(){
 	/***************************************/
 	// This code is from one of Microsoft's
 	// knowledge base articles, you can find it at
-    // http://support.microsoft.com/default.aspx?scid=KB;EN-US;q99261&
+	// http://support.microsoft.com/default.aspx?scid=KB;EN-US;q99261&
 	/***************************************/
 
-    COORD coordScreen = { 0, 0 };
+	COORD coordScreen = { 0, 0 };
 
 	BOOL bSuccess;
-    DWORD cCharsWritten;
-    CONSOLE_SCREEN_BUFFER_INFO csbi; /* to get buffer info */
-    DWORD dwConSize;
+	DWORD cCharsWritten;
+	CONSOLE_SCREEN_BUFFER_INFO csbi; /* to get buffer info */
+	DWORD dwConSize;
 
-    if(hOutput == NULL) return; 
+	if(hOutput == NULL) return; 
 
 	/* get the number of character cells in the current buffer */
 
-    bSuccess = GetConsoleScreenBufferInfo( hOutput, &csbi );
-    
+	bSuccess = GetConsoleScreenBufferInfo( hOutput, &csbi );
+	
 	if(!bSuccess) return;
-    
+	
 	dwConSize = csbi.dwSize.X * csbi.dwSize.Y;
 
-    /* fill the entire screen with blanks */
+	/* fill the entire screen with blanks */
 
-    bSuccess = FillConsoleOutputCharacter( hOutput, (TCHAR) ' ', dwConSize, coordScreen, &cCharsWritten );
-    if(!bSuccess) return;
-	    
+	bSuccess = FillConsoleOutputCharacter( hOutput, (TCHAR) ' ', dwConSize, coordScreen, &cCharsWritten );
+	if(!bSuccess) return;
+		
 
-    /* get the current text attribute */
+	/* get the current text attribute */
 
-    bSuccess = GetConsoleScreenBufferInfo( hOutput, &csbi );
-    if(!bSuccess) return;
-	    
+	bSuccess = GetConsoleScreenBufferInfo( hOutput, &csbi );
+	if(!bSuccess) return;
+		
 
-    /* now set the buffer's attributes accordingly */
+	/* now set the buffer's attributes accordingly */
 
-    bSuccess = FillConsoleOutputAttribute( hOutput, csbi.wAttributes, dwConSize, coordScreen, &cCharsWritten );
-    if(!bSuccess) return;
-	    
+	bSuccess = FillConsoleOutputAttribute( hOutput, csbi.wAttributes, dwConSize, coordScreen, &cCharsWritten );
+	if(!bSuccess) return;
+		
 
-    /* put the cursor at (0, 0) */
+	/* put the cursor at (0, 0) */
 
-    bSuccess = SetConsoleCursorPosition( hOutput, coordScreen );
-    if(!bSuccess) return;
-	    
+	bSuccess = SetConsoleCursorPosition( hOutput, coordScreen );
+	if(!bSuccess) return;
+		
 }
 
 BOOL show_console(){
-    CHECK_CONSOLE_HANDLE(hConsoleWnd);
-    return ShowWindow(hConsoleWnd,SW_SHOW);
+	CHECK_CONSOLE_HANDLE(hConsoleWnd);
+	return ShowWindow(hConsoleWnd,SW_SHOW);
 }
 
 BOOL hide_console(){
-    CHECK_CONSOLE_HANDLE(hConsoleWnd);
-    return ShowWindow(hConsoleWnd,SW_HIDE);
+	CHECK_CONSOLE_HANDLE(hConsoleWnd);
+	return ShowWindow(hConsoleWnd,SW_HIDE);
 }
 
 BOOL close_console(){
-    if (!FreeConsole()){
+	if (!FreeConsole()){
 		return FALSE;		
-    }
-    hOutput=NULL;
-    hInput=NULL;
-    hConsoleWnd=NULL;
-    *stdout=fOldOut;
-    *stdin=fOldIn;
-    return TRUE;
+	}
+	hOutput=NULL;
+	hInput=NULL;
+	hConsoleWnd=NULL;
+	*stdout=fOldOut;
+	*stdin=fOldIn;
+	return TRUE;
 };
 
 #endif
